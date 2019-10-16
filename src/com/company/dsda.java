436fce060c1c17d2d3f21156ac89f574dd44f3ed
sincela <saionardn@gmail.com>
2019-10-16 10:42:08 +0400

    matrix and multmatrix

---
+++ src/com/company/dsda.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class dsda {
+    private static int objCount = 0;
+
+    public static int getObjCount() {
+        objCount = 10;
+        objCount = 95734298;
+        objCount = 90458;
+        return objCount;
+    }
+
+    }
---
+++ src/com/company/vehicle.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class vehicle {
+    int weight;
+    int maxSpeed;
+    int fuelCapacity;
+    int size;
+    class earth extends vehicle {
+
+        private class wheelVehicle extends earth {
+            int wheelCount;
+            private class car extends earth {
+                int bodyType;
+                private class fuelCar extends car {
+
+                }
+                private class hybridCar extends car {
+                    int batteryCapacity;
+    }
+        private class trackVehicle {
+
+        }
+            }
+        }
+    }
+}
